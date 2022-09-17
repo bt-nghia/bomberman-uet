4f0824cfda50d59d016080f3901f6d1e82eccd7d
bt-nghia <nghiak99lhp@gmail.com>
2022-09-17 13:00:56 +0700

    add controller package

---
+++ src/uet/oop/bomberman/controller/PlayerController.java
@@ -0,0 +1,19 @@
+package uet.oop.bomberman.controller;
+
+public class PlayerController {
+    public static void moveDown() {
+
+    }
+
+    public static void moveUp() {
+
+    }
+
+    public static void moveRight() {
+
+    }
+
+    public static void moveLeft() {
+
+    }
+}
