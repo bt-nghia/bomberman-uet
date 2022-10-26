fedc7688fea730070954daf13beaa4cab70b44d6
bt-nghia <nghiak99lhp@gmail.com>
2022-10-26 09:43:36 +0700

    fix map block -> abstract

--- src/uet/oop/bomberman/entities/map/mapblock/MapBlock.java
+++ src/uet/oop/bomberman/entities/map/mapblock/MapBlock.java
@@ -3,7 +3,7 @@ package uet.oop.bomberman.entities.map.mapblock;
 import javafx.scene.image.Image;
 import uet.oop.bomberman.entities.Entity;
 
-public class MapBlock extends Entity {
+public abstract class MapBlock extends Entity {
     public MapBlock(int xUnit, int yUnit, Image img) {
         super(xUnit, yUnit, img);
     }
